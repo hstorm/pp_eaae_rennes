cf28de6478309ed31f94b962c7cfaa9d03e1629e
Hugo Storm <hugo.storm@ilr.uni-bonn.de>
2023-08-25 15:40:54 +0000

    - cleanup

--- examples/cumulative_prospect_theory.py
+++ examples/cumulative_prospect_theory.py
@@ -7,8 +7,6 @@
 # https://www.pnas.org/doi/full/10.1073/pnas.0806761106
 
 import os
-
-import os
 os.environ["CUDA_VISIBLE_DEVICES"]="1,2"
 
 import arviz as az
@@ -38,28 +36,18 @@ from numpyro.infer import Predictive, SVI, Trace_ELBO
 from numpyro.infer.autoguide import AutoLaplaceApproximation, AutoDiagonalNormal,AutoMultivariateNormal
 import numpyro.optim as optim
 
-# if "SVG" in os.environ:
-#     %config InlineBackend.figure_formats = ["svg"]
 az.style.use("arviz-darkgrid")
-# numpyro.set_platform("cpu")
-numpyro.set_platform("gpu")
+numpyro.set_platform("cpu")
+# numpyro.set_platform("gpu")
 
 # %%
 def load_data():
-    #%
     # import data
     df = pd.read_csv('https://raw.githubusercontent.com/paulstillman/thegreatstatsby/main/_posts/2021-03-08-ml-prospect/data_all_2021-01-08.csv')
-    df
-    # %
-    df['subject'].value_counts()
     
     # transform study in categorical variable, and get numeric categories
     df['study_cat'] = pd.Categorical(df['study']).codes
     
-    #%
-
-    df['gamble_type'].value_counts()
-    # %
     # count unique subjects by study
     df.groupby('study')['subject'].nunique()
     N = df.groupby('study')['subject'].nunique().sum()
@@ -80,10 +68,7 @@ def pyro_inference(rng_key,model, num_samples=200, num_warmup=1000, num_chains=2
     kernel = NUTS(model)
     mcmc = MCMC(kernel, num_samples=num_samples, num_warmup=num_warmup, num_chains=num_chains)
     mcmc.run(rng_key_, **kwargs)
-    
-    # Print summary
     mcmc.print_summary()
-    # Get samples
     samples = mcmc.get_samples()
     
     return mcmc, samples
@@ -100,7 +85,6 @@ def utility(x, lam, rho):
 def plot_utility(lam, rho, ax=None, addTitle=True):
     x_range = jnp.linspace(-10,10,20)
     util = utility(x_range,  lam=lam, rho=rho)
-    # print(util)
 
     if ax is None:
         f, ax = plt.subplots(figsize=(6, 6))
@@ -115,12 +99,6 @@ def plot_utility(lam, rho, ax=None, addTitle=True):
         plt.xlim([-12, 12])
         plt.ylim([-12, 12])
         plt.show()
-
-fig, ax = plt.subplots(figsize=(4, 3))
-plot_utility(2.6, .65, ax=ax)
-ax.set_ylim([-10, 10])
-ax.set_xlim([-10, 10])
-
 
 # %%
 # define a model for prospect theory in numpyro
@@ -143,41 +121,15 @@ def model_PT(gain, loss, cert, gamble_type, subject, took_gamble=None):
     numpyro.sample('took_gamble', dist.BernoulliProbs(p_accept), obs=took_gamble)
 
 # %%
-# TODO possible extension: transform model to a cumulative prospect theory model
-# could be based on https://ejwagenmakers.com/2011/NilssonEtAl2011.pdf
-
-# TODO possible extension: extent to hierarchical model, considering 
-# gamble_type and subject as random effects
-
-# TODO possible extension: adjust more to an agricultural context for example, 
-# an crop insurance model or a a technology adoption model  
-"""
-Two possible outcomes:
-1) Good weather, high yield, with probability p
-2) Bad weather, low yield, with probability 1-p
-
-Two possible technologies one with high investment one with 
-low investment (potentially the status quo). 
-
-yield_techNew[good weather, bad weather] = [120, 60]
-yield_techOld[good weather, bad weather] = [80, 60]
-costs[techNew, techOld] = [70, 20]
-utility_techNew[good weather, bad weather] = [120-70=50, 60-70=-10]
-utility_techOld[good weather, bad weather] = [80-20=60, 60-20=40]
-
-[Alternative Idea]
-Consider crop rotation choice under a cumulative prospect theory 
-perspective. Combination of certain 
-
-- We have distributions of prices for each crop
-- We have variable costs for each crop (depending on technology and farm size)
-- We have a distribution of yields for each crop, depending on weather and soil characteristics
-- We have positive/negative crop rotation yield effects 
-"""
-
-# %%
-
 if __name__ == "__main__":    
+    """
+    TODO possible extension: 
+        - transform model to a cumulative prospect theory model could be 
+          based on https://ejwagenmakers.com/2011/NilssonEtAl2011.pdf
+        - extent to hierarchical model, considering gamble_type and 
+          subject as random effects
+    """
+
     # %%
     # load data
     df, N, N_study, N_gainOnly, N_gainLoss = load_data()
@@ -200,10 +152,19 @@ if __name__ == "__main__":
                     #    study=df['study_cat'].values,
                        took_gamble=df['took_gamble'].values
                         )
+    # %%
+    # Illustrate utility function
+    fig, ax = plt.subplots(figsize=(4, 3))
+    plot_utility(2.6, .65, ax=ax)
+    # plot_utility(1., 0.5, ax=ax) # use prior extremes
+    # plot_utility(4, 1, ax=ax) # use prior extremes
+    ax.set_ylim([-10, 10])
+    ax.set_xlim([-10, 10])
     
     # %%
     # sample for prior
     prior_sam = sample_prior(rng_key, model_PT,**dat_X_train)
+
     # %%
     f, ax = plt.subplots(figsize=(6, 6))
     for i in range(0,200):
@@ -225,11 +186,6 @@ if __name__ == "__main__":
     print('Shape of took_gamble',prior_sam['took_gamble'].shape)
     plt.hist(prior_sam['took_gamble'][0,:10000], bins=100);
     
-    # %%
-    # plot utility function across a range of x values
-    x_range = jnp.linspace(-10,10,20)
-    # util = utility(x_range,  lam=1.4, rho=.83)
-
     # %%
     mcmc_M1, post_sam = pyro_inference(rng_key,model_PT,**dat_XY_train)
 
